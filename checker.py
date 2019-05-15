3a24ad68106d7ec380c38a4707c5070ed4034bf2
Nikhil Mathur <nikhilmathur95@gmail.com>
2019-05-15 16:31:58 -0400

    added countries code

--- checker.py
+++ checker.py
@@ -10,4 +10,14 @@ fs.LAND_AREA_SQ_MI = pandas.to_numeric(fs.LAND_AREA_SQ_MI)
 #Grab the two rows interested in and sort Land Area by Descending
 required = fs[['STATE', 'LAND_AREA_SQ_MI']]
 sortLandArea = required.sort_values('LAND_AREA_SQ_MI', ascending = False)
-print(sortLandArea) 
+#print(sortLandArea) 
+
+#Now do the same thing with countries
+c = pandas.read_csv("countries of the world.csv")
+c.rename(columns={'Country': 'COUNTRY', 'Area (sq. mi.)': 'LAND_AREA_SQ_MI'}, inplace = True)
+# ***The Area in this csv doesn't contain commas so nothing else needs to be done***
+
+required2 = c[['COUNTRY', 'LAND_AREA_SQ_MI']]
+sortLandArea2 = required2.sort_values('LAND_AREA_SQ_MI', ascending = False)
+print(sortLandArea2)
+
