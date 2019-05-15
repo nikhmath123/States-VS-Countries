4b3bcdbdbcdbd983d1d13d89db19bd0496882e55
Nikhil Mathur <nikhilmathur95@gmail.com>
2019-05-15 16:06:34 -0400

    cleaned and add comments

--- checker.py
+++ checker.py
@@ -1,12 +1,13 @@
 import csv
 import pandas
 
-fs_csv = pandas.read_csv("fifty-states.csv")
-fs_csv.rename(columns={'Land Area: Sq. Mi.': 'LAND_AREA_SQ_MI'}, inplace = True)
-fs_csv['LAND_AREA_SQ_MI'] = fs_csv['LAND_AREA_SQ_MI'].str.replace(",", "")
-fs_csv.LAND_AREA_SQ_MI = pandas.to_numeric(fs_csv.LAND_AREA_SQ_MI)
+#Read CSV and format data 
+fs = pandas.read_csv("fifty-states.csv")
+fs.rename(columns={'Land Area: Sq. Mi.': 'LAND_AREA_SQ_MI'}, inplace = True)
+fs.LAND_AREA_SQ_MI = fs.LAND_AREA_SQ_MI.str.replace(",", "")
+fs.LAND_AREA_SQ_MI = pandas.to_numeric(fs.LAND_AREA_SQ_MI)
 
-required = fs_csv[['STATE', 'LAND_AREA_SQ_MI']]
+#Grab the two rows interested in and sort Land Area by Descending
+required = fs[['STATE', 'LAND_AREA_SQ_MI']]
 sortLandArea = required.sort_values('LAND_AREA_SQ_MI', ascending = False)
-
 print(sortLandArea) 
